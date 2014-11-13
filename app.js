918c521d536aa2377759a972536580fbef4c11d3
adam lev <adam.inf@gmail.com>
2014-11-13 19:44:22 +0200

    initial upload

---
+++ app.js
@@ -0,0 +1,64 @@
+restaurants = 
+[
+	{
+		"name":"Sicilian",
+		"long":44,
+		"lat":44,
+		"dsc":"ad",
+		"tags":["vegan", "pork"]
+	},
+	{
+		"name":"hudson",
+		"long":44.2,
+		"lat":44.2,
+		"dsc":"addf",
+		"tags":["sabath", "pork"]
+	}
+]
+
+function updateMap()
+	{
+		var selectedTags = getSelectedTags();		
+		var results = [];
+		for (var i=0 ; i<restaurants.length ; i++) {
+			var currRestaurant = restaurants[i];
+			if (isKosher(selectedTags ,currRestaurant.tags))
+				{
+					results.push(currRestaurant);
+				}
+			
+		}
+		alert(results.length)
+	}
+
+
+function getSelectedTags()
+	{
+		var selected = [];
+		$('#checkboxes input:checked').each(function() {
+			selected.push($(this).val());
+		});
+		return selected;
+	}
+
+	
+function isKosher(selectedTags ,restaurantTags)
+{
+		for (var i=0; i<selectedTags.length ; i++){
+			
+			if (!inCollection(selectedTags[i], restaurantTags))
+			{
+				return false;
+			}
+		}
+		return true;
+}
+
+function inCollection(item, collection)
+{
+	for (var i = 0 ; i<collection.length ; i++)
+	{
+		if (item == collection[i]) {return true;}
+	}
+	return false;
+}

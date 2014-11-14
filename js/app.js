55c8a834ed50626443b8892bcb5ec5c167dd76cb
Taiwaninja <gokosss@hotmail.com>
2014-11-14 06:50:08 +0200

    Updated Paths

--- js/app.js
+++ js/app.js
@@ -6,14 +6,14 @@ restaurants =
 		"long":34.840348,
 		"lat":32.110799,
 		"dsc":"Sicilian",
-		"icon": "./grapics/40x40pin.png",
+		"icon": "../grapics/40x40pin.png",
 		"tags":["no_slaves","also_vegan","sign_lang"]
 	},
 	{
 		"name":"Hudson",
 		"long":34.840248,
 		"lat":32.109799,		
-		"icon": "./grapics/40x40pin.png",
+		"icon": "../grapics/40x40pin.png",
 		"dsc":"Hudson",
 		"tags":["no_slaves","social_tag"]
 	},
@@ -22,7 +22,7 @@ restaurants =
 	"lat":32.109132,
 	"long":34.84078,
 	"dsc":"בודהה בורגר",
-		"icon": "./grapics/40x40pin.png",
+		"icon": "../grapics/40x40pin.png",
 	"tags":["no_slaves","wheelchair","vegan","kosher","social_tag"]
 	},	
 	{
@@ -30,7 +30,7 @@ restaurants =
 	"lat":32.109132,
 	"long":34.84058,
 	"dsc":"<rtl>מוזס רמת החייל Tel Aviv<rtl/>",
-	"icon": "./grapics/moses.png",
+	"icon": "../grapics/moses.png",
 	"tags":["no_slaves","wheelchair"]
 	},	
 	{
@@ -38,7 +38,7 @@ restaurants =
 	"lat":32.113464, 
 	"long": 34.840457, 
 	"dsc":"Kkao",	
-	"icon": "./grapics/40x40pin.png",
+	"icon": "../grapics/40x40pin.png",
 	"tags":["no_slaves","shabbath","also_vegan","kosher_compiler","kosher"]
 	},
 	{
@@ -46,7 +46,7 @@ restaurants =
 	"lat":32.109328, 
 	"long": 34.839820,
 	"dsc":"Max Brener",	
-	"icon": "./grapics/40x40pin.png",
+	"icon": "../grapics/40x40pin.png",
 	"tags":["no_slaves","kosher"]
 	},
 	{
@@ -54,7 +54,7 @@ restaurants =
 	"lat":32.109625,
 	"long": 34.840444,
 	"dsc":"Arcaffe",	
-	"icon": "./grapics/40x40pin.png",
+	"icon": "../grapics/40x40pin.png",
 	"tags":["shabbath","also_vegan","kosher_compiler","kosher"]
 	},
 	{
@@ -62,7 +62,7 @@ restaurants =
 	"lat":32.109825,
 	"long": 34.840644,
 	"dsc":"Dabush Shawarma",	
-	"icon": "./grapics/40x40pin.png",
+	"icon": "../grapics/40x40pin.png",
 	"tags":["shabbath","also_vegan","kosher_compiler","kosher"]
 	},
 	{
@@ -70,7 +70,7 @@ restaurants =
 	"lat":32.109025,
 	"long": 34.840044,
 	"dsc":"AM:PM",	
-	"icon": "./grapics/40x40pin.png",
+	"icon": "../grapics/40x40pin.png",
 	"tags":["also_vegan","no_slaves"]
 	}
 ]
